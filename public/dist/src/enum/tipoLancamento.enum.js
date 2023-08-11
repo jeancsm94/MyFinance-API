97af2a31ae23eeb811a517d5beca25703bb924e1
jeancsm94 <jean0king@gmail.com>
2023-08-11 10:32:43 -0300

    Ajuste versao

--- public/dist/src/enum/tipoLancamento.enum.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.tipoLancamento = void 0;
-var tipoLancamento;
-(function (tipoLancamento) {
-    tipoLancamento[tipoLancamento["Receita"] = 0] = "Receita";
-    tipoLancamento[tipoLancamento["Despesa"] = 1] = "Despesa";
-})(tipoLancamento || (exports.tipoLancamento = tipoLancamento = {}));
--- public/dist/src/services/mongo/collections/collections.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.tipoLancamentoCollection = exports.lancamentoCollection = void 0;
-exports.lancamentoCollection = { name: 'lancamentos' };
-exports.tipoLancamentoCollection = { name: 'tipoLancamentos' };
