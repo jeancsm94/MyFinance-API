9b946567e884fa3fdd097bde3122def79da22803
Jean Carlos dos Santos Maidana <jean0king@gmail.com>
2023-08-11 22:21:51 -0300

    Ajuste request cors

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,18 @@ app.initApp();
 
 
 server.use(express.json());
+server.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+    );
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-type, Accept, Authorization"
+    );
+    next();
+});
 
 server.get("/api/", (req: Request, res: Response) => {
     res.json('Hello ')
